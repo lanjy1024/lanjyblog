7301a5ec4621c15cc39f3547534c2ceead124dce
lanjy1024 <1615960786@qq.com>
2020-03-11 11:58:45 +0800

    springboot版本升级到2.0,访问静态资源问题的解决，jpa懒加载的问题

--- src/main/java/com/lanjy/blog/dao/TagRepository.java
+++ src/main/java/com/lanjy/blog/dao/TagRepository.java
@@ -1,9 +1,9 @@
 package com.lanjy.blog.dao;
 
 import com.lanjy.blog.po.Tag;
-import com.lanjy.blog.po.Type;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -15,11 +15,12 @@ import java.util.List;
  * @创建人：lanjy
  * @创建时间：2020/1/13
  */
-public interface TagRepository extends JpaRepository<Tag,Long>{
+public interface TagRepository extends JpaRepository<Tag,Long>,JpaSpecificationExecutor<Tag> {
 
     Tag findTagByName(String name);
 
     @Query("select t from Tag t")
     List<Tag> findTop(Pageable pageable);
-
+    @Query("select t from Tag t where t.id in ?1")
+    List<Tag> findByIds(List<Long> longs);
 }
